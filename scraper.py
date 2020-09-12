157c5b338ead119c9ab3bb164c6faa8271439eb8
sohumgala <70659668+sohumgala@users.noreply.github.com>
2020-09-12 01:39:13 -0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,6 +1,7 @@
 import requests
 import lxml.html as lh
 import pandas as pd
+import matplotlib.pyplot as plt
 
 url = 'http://rha.gtorg.gatech.edu/treats-testing'
 page = requests.get(url)
@@ -16,32 +17,38 @@ for t in tr_elements[0]:
     col.append((name,[]))
 
 for j in range(1,len(tr_elements)):
-    #T is our j'th row
     T=tr_elements[j]
     
-    #If row is not of size 10, the //tr data is not from our table 
     if len(T)!=8:
         break
-    
-    #i is the index of our column
+
     i=0
-    
-    #Iterate through each element of the row
+
     for t in T.iterchildren():
         data=t.text_content() 
-        #Check if row is empty
         if i>0:
-        #Convert any numerical value to integers
             try:
                 data=int(data)
             except:
                 pass
-        #Append the data to the empty list of the i'th column
         col[i][1].append(data)
-        #Increment i for the next column
         i+=1
-
-
 
 Dict={title:column for (title,column) in col}
 df=pd.DataFrame(Dict)
+
+df.to_csv('test.csv')
+
+df = pd.read_csv('test.csv')
+
+for i in range(len(df)):
+    df.loc[i, "Average"] = df.loc[i, "Average"][:-1]
+
+df2 = df[["Rank*", "Residence Hall", "Average"]].copy()
+df = df2
+for i in range(len(df)):
+    df.loc[i, "Average"] = int(df.loc[i, "Average"])
+
+df.plot(x = 'Residence Hall', y = 'Average', kind = 'bar', title = "Average Participation by Residence Hall", legend = "False")
+plt.ylabel("Average")
+plt.tight_layout()
